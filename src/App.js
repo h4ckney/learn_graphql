4a732d1d82022452491f973aadb87add80910f87
MacBook_Pro <jmyoon555@gmail.com>
2022-04-22 09:57:52 +0900

    apollo and graphql init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+
+import Continents from './container/Continents'
+
+const client = new ApolloClient({
+    link: createHttpLink({ uri: "https://countries.trevorblades.com" }),
+    cache: new InMemoryCache()
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <h1>React + Apollo Hooks</h1>
+            <Continents />
+        </ApolloProvider>
+    );
 }
 
 export default App;
